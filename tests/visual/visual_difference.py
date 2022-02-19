7787cbb95feed41368a68a72ca18d2fc51867229
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-02-19 22:06:45 +0000

    tmp

---
+++ tests/visual/visual_difference.py
@@ -0,0 +1,7 @@
+import argparse
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base-ref", type=str, help="Git ref for ")
