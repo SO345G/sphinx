2ef767a0425f2218dbec4b47c7f1b05facdf77b1
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-03-05 12:42:17 +0000

    tmp

--- sphinx/util/_image_size.py
+++ sphinx/util/_image_size.py
@@ -11,10 +11,10 @@ if TYPE_CHECKING:
 try:
     import PIL.Image
 except ImportError:
-    def _get_image_size_pil(filename: str) -> tuple[int, int] | None:
+    def _get_image_size_pil(filename: str, /) -> tuple[int, int] | None:
         return None
 else:
-    def _get_image_size_pil(filename: str) -> tuple[int, int] | None:
+    def _get_image_size_pil(filename: str, /) -> tuple[int, int] | None:
         try:
             with PIL.Image.open(filename) as im:
                 return im.size
@@ -39,25 +39,38 @@ def _get_image_size(path: os.PathLike | str) -> tuple[int, int]:
     with open(path, "rb") as file:
         head = file.read(24)
         size = len(head)
+
         # handle GIFs
-        if size >= 10 and head[:6] in {b'GIF87a', b'GIF89a'}:
+        if head.startswith((b'GIF87a', b'GIF89a')):
             return _gif(head, size)
 
-        # see png edition spec bytes are below chunk length then and finally the
-        if size >= 24 and head.startswith(b'\211PNG\r\n\032\n') and head[12:16] == b'IHDR':
-            return _png(head)
-
-        # Maybe this is for an older PNG version.
-        if size >= 16 and head.startswith(b'\211PNG\r\n\032\n'):
-            return _old_png(head)
+        # handle PNGs
+        if head.startswith(b'\211PNG\r\n\032\n'):
+            return _png(head, size)
 
         # handle JPEGs
-        if size >= 2 and head.startswith(b'\377\330'):
-            return _jpeg(file)
+        if head.startswith(b'\377\330'):
+            try:
+                file.seek(0)  # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf or ftype in [0xc4, 0xc8, 0xcc]:
+                    file.seek(size, 1)
+                    byte = file.read(1)
+                    while ord(byte) == 0xff:
+                        byte = file.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', file.read(2))[0] - 2
+                # We are at a SOFn block
+                file.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', file.read(4))
+            except (struct.error, TypeError):
+                raise ValueError("Invalid JPEG file")
 
         # handle JPEG2000s
         if size >= 12 and head.startswith(b'\x00\x00\x00\x0cjP  \r\n\x87\n'):
-            return _jpeg2000(file)
+            file.seek(48)
+            return _jpeg2000(file.read(8))
 
         # handle big endian TIFF
         if size >= 8 and head.startswith(b"\x4d\x4d\x00\x2a"):
@@ -76,27 +89,27 @@ def _get_image_size(path: os.PathLike | str) -> tuple[int, int]:
                         raise ValueError(msg)
                 elif tag == 257:
                     if datatype == 3:
-                        height = int(data / 65536)
+                        height = int(data / 2**16)
                     elif datatype == 4:
                         height = data
                     else:
-                        msg = "Invalid TIFF file: " \
-                              "height column data type should be SHORT/LONG."
+                        msg = ("Invalid TIFF file: "
+                               "height column data type should be SHORT/LONG.")
                         raise ValueError(msg)
                 if width != -1 and height != -1:
                     break
             if width == -1 or height == -1:
-                msg = "Invalid TIFF file: " \
-                      "width and/or height IDS entries are missing."
+                msg = ("Invalid TIFF file: "
+                       "width and/or height IDS entries are missing.")
                 raise ValueError(msg)
             return width, height
 
         # handle big endian TIFF
-        if size >= 8 and head.startswith(b"\x49\x49\x2a\x00"):
+        if head.startswith(b"\x49\x49\x2a\x00"):
             offset = struct.unpack('<L', head[4:8])[0]
             file.seek(offset)
-            ifdsize = struct.unpack("<H", file.read(2))[0]
-            for _ in range(ifdsize):
+            ifd_size = struct.unpack("<H", file.read(2))[0]
+            for _ in range(ifd_size):
                 tag, datatype, count, data = struct.unpack("<HHLL", file.read(12))
                 if tag == 256:
                     width = data
@@ -110,16 +123,16 @@ def _get_image_size(path: os.PathLike | str) -> tuple[int, int]:
             return width, height
 
         # handle little endian BigTiff
-        if size >= 8 and head.startswith(b"\x49\x49\x2b\x00"):
+        elif head.startswith(b"\x49\x49\x2b\x00"):
             bytesize_offset = struct.unpack('<L', head[4:8])[0]
             if bytesize_offset != 8:
-                msg = f'Invalid BigTIFF file: ' \
-                      f'Expected offset to be 8, found {bytesize_offset} instead.'
+                msg = ('Invalid BigTIFF file: '
+                       f'Expected offset to be 8, found {bytesize_offset} instead.')
                 raise ValueError(msg)
             offset = struct.unpack('<Q', head[8:16])[0]
             file.seek(offset)
-            ifdsize = struct.unpack("<Q", file.read(8))[0]
-            for _i in range(ifdsize):
+            ifd_size = struct.unpack("<Q", file.read(8))[0]
+            for _ in range(ifd_size):
                 tag, datatype, count, data = struct.unpack("<HHQQ", file.read(20))
                 if tag == 256:
                     width = data
@@ -133,13 +146,18 @@ def _get_image_size(path: os.PathLike | str) -> tuple[int, int]:
             return width, height
 
         # handle SVGs
-        if size >= 5 and (head.startswith((b'<?xml', b'<svg'))):
-            return _svg(head, size, file)
+        if head.startswith((b'<?xml', b'<svg')):
+            return _svg(head + file.read(992))
+
+        if head.startswith(b'RIFF') and head[8:12] == b'WEBP':
+            return _webp(head)
 
     return -1, -1
 
 
 def _gif(head: bytes, size: int) -> tuple[int, int]:
+    if size < 10:
+        raise ValueError("Invalid GIF file")
     # Check to see if content_type is correct
     try:
         width, height = struct.unpack("<hh", head[6:10])
@@ -148,15 +166,15 @@ def _gif(head: bytes, size: int) -> tuple[int, int]:
         raise ValueError("Invalid GIF file")
 
 
-def _png(head: bytes) -> tuple[int, int]:
-    try:
-        width, height = struct.unpack(">LL", head[16:24])
-        return width, height
-    except struct.error:
+def _png(head: bytes, size: int) -> tuple[int, int]:
+    if size < 24:
         raise ValueError("Invalid PNG file")
-
-
-def _old_png(head: bytes) -> tuple[int, int]:
+    if head[12:16] == b'IHDR':
+        try:
+            width, height = struct.unpack(">LL", head[16:24])
+            return width, height
+        except struct.error:
+            raise ValueError("Invalid PNG file")
     # Check to see if we have the right content type
     try:
         width, height = struct.unpack(">LL", head[8:16])
@@ -185,21 +203,15 @@ def _jpeg(file: io.BufferedReader) -> tuple[int, int]:
         raise ValueError("Invalid JPEG file")
 
 
-def _jpeg2000(file: io.BufferedReader) -> tuple[int, int]:
-    file.seek(48)
+def _jpeg2000(data: bytes) -> tuple[int, int]:
     try:
-        height, width = struct.unpack('>LL', file.read(8))
+        height, width = struct.unpack('>LL', data)
         return width, height
     except struct.error:
         raise ValueError("Invalid JPEG2000 file")
 
 
-def _tiff_be_4d(head: bytes, size: int, file: io.BufferedReader) -> tuple[int, int]: ...
-def _tiff_be_49(head: bytes, size: int, file: io.BufferedReader) -> tuple[int, int]: ...
-def _tiff_le(head: bytes, size: int, file: io.BufferedReader) -> tuple[int, int]: ...
-def _svg(head: bytes, size: int, file: io.BufferedReader) -> tuple[int, int]:
-    file.seek(0)
-    data = file.read(1024)
+def _svg(data: bytes) -> tuple[int, int]:
     try:
         start = data.decode('utf-8')
         width = re.search(r'[^-]width="(.*?)"', start).group(1)
@@ -209,6 +221,22 @@ def _svg(head: bytes, size: int, file: io.BufferedReader) -> tuple[int, int]:
     width = int(_convert_to_px(width))
     height = int(_convert_to_px(height))
     return width, height
+
+
+def _webp(head: bytes) -> tuple[int, int]:
+    if head[12:16] == b"VP8 ":
+        width, height = struct.unpack("<HH", head[26:30])
+        return width, height
+    if head[12:16] == b"VP8X":
+        width = struct.unpack("<I", head[24:27] + b"\0")[0]
+        height = struct.unpack("<I", head[27:30] + b"\0")[0]
+        return width, height
+    if head[12:16] == b"VP8L":
+        b = head[21:25]
+        width = (((b[1] & 63) << 8) | b[0]) + 1
+        height = (((b[3] & 15) << 10) | (b[2] << 2) | ((b[1] & 192) >> 6)) + 1
+        return width, height
+    raise ValueError("Unsupported WebP file")
 
 
 def _convert_to_px(value: str) -> float:
