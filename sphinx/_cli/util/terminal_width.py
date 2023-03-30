981c6bd955ddb8a888620884b86462c4244be5d7
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-03-30 19:11:27 +0100

    Refactor ``_cli.util.terminal_width``
    - Use ``re.Pattern``
    - Avoid importing ``shutil`` for speed
    - Compare raw and formatted strings in ``term_width_line``

--- sphinx/_cli/util/terminal_width.py
+++ sphinx/_cli/util/terminal_width.py
@@ -2,24 +2,32 @@
 
 from __future__ import annotations
 
-import re
-import shutil
-
-_ansi_re: re.Pattern = re.compile('\x1b\\[(\\d\\d;){0,2}\\d\\dm')
+import os
+import sys
 
 
 def get_terminal_width() -> int:
     """Return the width of the terminal in columns."""
-    return shutil.get_terminal_size().columns - 1
+    try:
+        columns = int(os.environ.get('COLUMNS', 0))
+    except ValueError:
+        columns = 0
+    if columns > 1:
+        return columns - 1
+    try:
+        return os.get_terminal_size(sys.__stdout__.fileno()).columns - 1
+    except (AttributeError, ValueError, OSError):
+        # fallback
+        return 80 - 1
 
 
 _tw: int = get_terminal_width()
 
 
-def term_width_line(text: str) -> str:
-    if not codes:
+def term_width_line(formatted_text: str, raw_text: str) -> str:
+    if formatted_text == raw_text:
         # if no coloring, don't output fancy backspaces
-        return text + '\n'
+        return formatted_text + '\n'
     else:
         # codes are not displayed, this must be taken into account
-        return text.ljust(_tw + len(text) - len(_ansi_re.sub('', text))) + '\r'
+        return formatted_text.ljust(_tw + len(formatted_text) - len(raw_text)) + '\r'
