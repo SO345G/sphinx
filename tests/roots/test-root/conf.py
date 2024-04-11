5f6aec897b8b948a1afe85dd307921e25f0cf7d5
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-04-12 00:48:40 +0100

    strip

---
+++ tests/roots/test-root/conf.py
@@ -0,0 +1,13 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.extlinks',
+]
+
+project = 'Sphinx <Tests>'
+copyright = '1234-6789, copyright text credits'
+version = '0.6'
+numfig = True
+
+html_last_updated_fmt = '%b %d, %Y'
